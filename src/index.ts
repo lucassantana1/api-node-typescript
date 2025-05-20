623a3d67b07c774c3906f050a037fcacdd3a50db
Lucas Santana <lucasdeoliveirasantana@gmail.com>
2025-05-20 17:50:12 -0300

    add dotenv and refactoring url mongo

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -22,3 +26,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
+
+const MONGO_URL = process.env.MONGO_URL!;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
