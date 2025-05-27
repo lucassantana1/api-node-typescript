4424b1c99792effd638bab6f4c037c55058cd823
Lucas Santana <lucasdeoliveirasantana@gmail.com>
2025-05-27 09:11:44 -0300

    middleware creation

---
+++ src/middlewares/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { get, identity, merge } from "lodash";
+
+import { getUserBySessionToken } from "../db/users";
+
+export const isAutheticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["LUCAS-AUTH"];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
